ff0037e3a0e28c4695c4d5af5d72e4c38255a12d
Swarajpaul2001 <92090492+Swarajpaul2001@users.noreply.github.com>
2022-12-06 15:54:06 +0530

    Update doa.java

--- Spring Assignment/Dao/doa.java
+++ Spring Assignment/Dao/doa.java
@@ -1,1 +1,20 @@
+public class UserDao implements Dao&lt;User&gt; {
 
+private List&lt;User&gt; users = new ArrayList&lt;&gt;();
+
+public UserDao() {
+users.add(new User(&quot;John&quot;, &quot;john@domain.com&quot;));
+users.add(new User(&quot;Susan&quot;, &quot;susan@domain.com&quot;));
+}
+
+@Override
+public Optional&lt;User&gt; get(long id) {
+return Optional.ofNullable(users.get((int) id));
+}
+
+@Override
+public List&lt;User&gt; getAll() {
+return users;
+}
+
+@Override
